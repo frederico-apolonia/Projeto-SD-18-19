35a2c440614d8c913571e3b95ece74d411f16a81
Frederico Apolónia <fredericojcapolo@gmail.com>
2018-09-29 18:08:01 +0100

    Initial commit

---
+++ include/table-private.h
@@ -0,0 +1,17 @@
+#ifndef _TABLE_PRIVATE_H
+#define _TABLE_PRIVATE_H
+
+#include "list.h"
+
+struct table_t {
+
+};
+
+/* Função para criar/inicializar uma nova tabela hash, com n linhas 
+ * (módulo da função HASH).
+ */
+struct table_t *table_create(int n);
+
+void table_print(struct table_t *table);
+
+#endif
