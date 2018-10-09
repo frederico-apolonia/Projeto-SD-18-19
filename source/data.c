1243d0014fddc5d6b8324f799a2bb6da85879089
Frederico Apolónia <fredericojcapolo@gmail.com>
2018-10-09 23:25:42 +0100

    Fixed data.c errors. Now passes all test_data.c tests!

--- source/data.c
+++ source/data.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "data.h"
 
 struct data_t *data_create(int size){
+	if(size <= 0) {
+		return NULL;
+	}
 	struct data_t *newData = (struct data_t *) malloc(sizeof(struct data_t));
 	if(newData == NULL) {
 		return NULL; // if malloc failed
@@ -20,11 +24,20 @@ struct data_t *data_create(int size){
 
 struct data_t *data_create2(int size, void *data){
 	struct data_t *newData = data_create(size);
-	if(newData == NULL) {
+	if(newData == NULL || data == NULL) {
 		free(newData);
 		return NULL;
 	}
-	memcpy(newData->data, data, size);
+	newData -> data = data;
+	/*
+	"Não. Para duplicar uma estrutura data usam a fução data_dup(). 
+	A função data_create2() apenas aloca memória para a estrutura, 
+	mas não para os dados apontados pelo campo data. 
+	Este campo fica com o ponteiro passado como parâmetro 
+	(e portanto esse ponteiro tem de apontar para uma zona de memória 
+	já alocada e com os dados) ."
+	
+	memcpy(newData->data, data, size);*/
 	return newData;
 }
 
@@ -40,5 +53,21 @@ void data_destroy (struct data_t *data){
 }
 
 struct data_t *data_dup(struct data_t *data){
-	return data == NULL ? NULL : data_create2(data->datasize, data->data);
+	if(data == NULL) {
+		return NULL;
+	}
+
+	struct data_t *data_copy;
+	if ((data_copy = data_create(data->datasize)) != NULL ) {
+		// memoria bem alocada
+		if (data->data == NULL) {
+			// se o data for nulo, nao existe nada para copiar
+			free(data_copy);
+			return NULL;
+		}
+		memcpy(data_copy->data, data->data, data->datasize);
+		return data_copy;
+	}
+	
+	return NULL;
 }
