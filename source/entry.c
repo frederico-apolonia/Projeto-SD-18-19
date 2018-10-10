4c4a23c92bdd41da837a8c12af56a25175c140b8
Frederico Apolónia <fc47892@alunos.fc.ul.pt>
2018-10-10 12:55:16 +0100

    Fixed entry.c; Now passes all tests!

--- source/entry.c
+++ source/entry.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "entry.h"
 #include "data.h"
 
@@ -17,6 +18,10 @@ struct entry_t *entry_create(char *key, struct data_t *data){
 
 void entry_destroy(struct entry_t *entry){
 	
+	if(entry == NULL) {
+		return;
+	}
+
 	if(entry->value != NULL){
 		data_destroy(entry->value);
 	}
@@ -31,8 +36,22 @@ struct entry_t *entry_dup(struct entry_t *entry){
 	if(entry == NULL){
 		return NULL;
 	}
-	struct entry_t *dupEntry = entry_create(entry->key,entry->value);
+	struct entry_t *dupEntry = (struct entry_t *) malloc(sizeof(struct entry_t));
 	if(dupEntry == NULL){
+		return NULL;
+	}
+
+	int key_size = strlen(entry->key)+1; // \0
+	dupEntry->key = (char*) malloc(key_size);
+	if(dupEntry->key == NULL) {
+		free(dupEntry);
+		return NULL;
+	}
+	strcpy(dupEntry->key, entry->key);
+	dupEntry->value = data_dup(entry->value);
+	if(dupEntry->value == NULL) {
+		free(dupEntry->key);
+		free(dupEntry);
 		return NULL;
 	}
 	return dupEntry;
